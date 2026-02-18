0d199eb600911492f180f1ac474c231fcd36a87b
madrabour <madrabour@gmail.com>
2026-02-18 17:09:12 +0100

    Second Loop Test cut fps

--- main.py
+++ main.py
@@ -2,23 +2,27 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
+
 def main():
     pygame.init()
-    #print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    fps_clock = pygame.time.Clock()
+    dt = 0
+    
 
     while True:
         log_state()
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
         screen.fill("black")
         pygame.display.flip()
+        
+        last_call_time = fps_clock.tick(60)
+        dt = last_call_time / 1000
 
 
 if __name__ == "__main__":
